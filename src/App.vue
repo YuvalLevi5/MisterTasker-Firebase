821250038e71f7f8c88b047f3f6db6c9d01f4109
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-08-30 18:09:27 +0300

    deploy

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <app-header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+  import appHeader from './cmps/app-header.vue'
+  export default {
+    created() {
+      this.$store.dispatch({type: 'loadTasks'})
+    },
+    components: {appHeader},
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/cmps/app-header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header-wrapper ">
+    <div  class="main-header main-layout-container">
+      <div class="">
+        <h1 @click="goHome" class="logo">
+          <span class="clr-teal">Mister</span>Tasker
+        </h1>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: 'AppHeader',
+  components: {},
+  data() {
+    return {}
+  },
+  created() { },
+  methods: {
+    goHome() {
+      console.log('run')
+      this.$router.push('/')
+    },
+  },
+  computed: {},
+  unmounted() { },
+}
+</script>
+<style>
+</style>
